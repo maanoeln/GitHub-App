1ef2c827475265818267fd751a5378f5a03a1df6
Manoel Neto <ManoelNeto@MacBook-Pro.local>
2021-04-11 21:03:40 -0300

    feat: add app container to render in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import AppContainer from './containers/AppContainer';
+import GlobalStyled from './GlobalStyled';
 
-const App = () => <div />;
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const ComponentsContainer = styled.div`
+  width: 50%;
+`;
+
+const App = () => (
+  <Container>
+    <GlobalStyled />
+    <ComponentsContainer>
+      <AppContainer />
+    </ComponentsContainer>
+  </Container>
+);
 
 export default App;
